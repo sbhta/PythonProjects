28c37c431374e75d0dce32709b66f1e5c11a245f
sbhta <68438322+sbhta@users.noreply.github.com>
2021-05-11 10:32:57 +0300

    _

---
+++ Duraka/socket/server.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import time
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+        msg = conn.recv()
+
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] starting the server")
+start()
