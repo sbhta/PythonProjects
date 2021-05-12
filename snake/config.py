0b3e1ad93fbf2511d1f952b468ef10b57424ab61
sbhta <68438322+sbhta@users.noreply.github.com>
2021-05-12 19:44:09 +0300

    a

--- snake/config.py
+++ snake/config.py
@@ -5,11 +5,17 @@ class s_part:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.dirX = 1
-        self.dirY = 1
-
+        self.dirX = 0
+        self.dirY = 0
+        self.oldX = self.x
+        self.oldY = self.y
     def draw(self, screen):
         pygame.draw.rect(screen, "white", (self.x, self.y, 16, 16))
+
+    def move_p(self, p):
+        self.x = p.oldX
+        self.y = p.oldY
+        print(f"{self.x} {self.y}")
 
 
 class head(s_part):
--- snake/game.py
+++ snake/game.py
@@ -8,27 +8,36 @@ screen_y = 670
 screen = pygame.display.set_mode((screen_x, screen_y))
 
 snake_size = 1
-snake_array = [head(360, 335)]
+snake_array = [s_part(328, 335),s_part(344, 335),head(360, 335)]
 
+clock = pygame.time.Clock()
+tps = 5
 while True:
+    clock.tick(tps)
+    screen.fill("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                snake_array[0].dirY = 0
-                if snake_array[0].x != -1: snake_array[0].dirX = 1
+                snake_array[-1].dirY = 0
+                if snake_array[-1].x != -1: snake_array[-1].dirX = 1
             elif event.key == pygame.K_LEFT:
-                snake_array[0].dirY = 0
-                if snake_array[0].x != 1: snake_array[0].dirX = -1
-            elif event.key == pygame.K_RIGHT:
-                snake_array[0].dirY = 0
-                if snake_array[0].x != -1: snake_array[0].dirX = 1
-            elif event.key == pygame.K_RIGHT:
-                snake_array[0].dirY = 0
-                if snake_array[0].x != -1: snake_array[0].dirX = 1
+                snake_array[-1].dirY = 0
+                if snake_array[-1].x != 1: snake_array[-1].dirX = -1
+            elif event.key == pygame.K_DOWN:
+                snake_array[-1].dirX = 0
+                if snake_array[-1].y != -1: snake_array[-1].dirY = 1
+            elif event.key == pygame.K_UP:
+                snake_array[-1].dirX = 0
+                if snake_array[-1].y != 1: snake_array[-1].dirY = -1
 
-    for part in snake_array:
-            part.draw(screen)
+    snake_array[-1].move()
+    for i in snake_array:
+        try: i.move_p(snake_array[snake_array.index(i) + 1])
+        except: print("fin")
+
+    for i in snake_array:
+        i.draw(screen)
 
     pygame.display.update()
